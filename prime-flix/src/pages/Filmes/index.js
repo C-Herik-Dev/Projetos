9241607a6ef5397215ad07b431d1b4a8e7bad982
Carlos Herik <carlosherik.dev@gmail.com>
2025-10-13 21:35:05 -0300

    Filmes

--- prime-flix/src/pages/Filmes/index.js
+++ prime-flix/src/pages/Filmes/index.js
@@ -1,8 +1,64 @@
+import { useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import './filmes-info.css'
 
-function Filmes(){
+import api from '../../services/api';
+
+function Filmes (){
+  const {id} = useParams();
+  const [filmes, setFilmes] = useState({});
+  const [loading, setLoading] = useState(true);
+  
+  useEffect(()=>{
+    async function loadFilmes(){
+      await api.get(`/movie/${id}`, {
+        params:{
+          api_key: "705236d5b5522ebbe290f28f32043592",
+          language: "pt-BR",
+        }
+      })
+      .then((response)=>{
+        setFilmes(response.data);
+        setLoading(false);
+      })
+      .catch(()=>{
+        console.log("FILME NÂO ENCONTRADO")
+      })
+    }
+
+    loadFilmes();
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO")
+    }
+  }, [])
+
+  if(loading){
+    return(
+      <div className="filmes-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    )
+  }
+  
   return(
-    <h1>Pagina de filmes</h1>
+    <div className="filmes-info">
+      <h1>{filmes.title}</h1>
+      <img src={`https://image.tmdb.org/t/p/original/${filmes.backdrop_path}`} alt={filmes.title}/>
 
+      <h3>Sinopse</h3>
+      <span>{filmes.overview}</span>
+      <strong>Avaliação: {filmes.vote_average.toFixed(1)} /10</strong>
+
+      <div className='area-buttons'>
+        <button>Salvar</button>
+        <button>
+          <a href="#">
+            Trailer
+          </a>
+        </button>
+      </div>
+    </div>
   )
 }
 
--- prime-flix/src/pages/Home/index.js
+++ prime-flix/src/pages/Home/index.js
@@ -40,12 +40,12 @@ function Home(){
   return(
     <div className="container">
       <div className="lista-filmes">
-         {filmes.map((filme)=>{
+         {filmes.map((filmes)=>{
             return(
-              <article key={filme.id}>
-                <strong>{filme.title}</strong>
-                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
-                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+              <article key={filmes.id}>
+                <strong>{filmes.title}</strong>
+                <img src={`https://image.tmdb.org/t/p/original/${filmes.poster_path}`} alt={filmes.title}/>
+                <Link to={`/filmes/${filmes.id}`}>Acessar</Link>
               </article>
             )
          })}
