c8fb2ef99ea2ca0b84a6806d597ec31549b173f1
Carlos Herik <carlosherik.dev@gmail.com>
2025-10-14 21:10:59 -0300

    Ajustes no projeto

--- prime-flix/src/components/Header/index.js
+++ prime-flix/src/components/Header/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function Header(){
   return(
     <header>
-      <Link className="logo" to="/">Prime Flix</Link>
+      <Link className="logo" to="/" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>Prime Flix</Link>
       <Link className="favoritos" to="/favoritos">Meus Filmes</Link>
     </header>
   )
